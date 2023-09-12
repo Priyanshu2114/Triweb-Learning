de4c3f283643a7148e34ff7a6f12eeb1be504cf6
Priyanshu Singh <psjnva@gmail.com>
2023-09-12 23:42:11 +0200

    Number Comparison

---
+++ JavaScript/concept4.js
@@ -0,0 +1,20 @@
+var a = 5 ;
+var b = 10 ;
+var c = 15 
+
+if (a > b ) {
+    console.log("a is greater than b");
+
+    }
+
+    if(c > b) {
+        console.log("c is greater than all")
+    }
+
+      else {
+    console.log("b is only greater than a");
+}
+
+if (c > b) {
+    console.log("C is great than b")
+}
